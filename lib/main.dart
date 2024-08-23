4922d0fefed9e70bea180fb535ebf2d5dc9a76de
Juan Uys <opyate@gmail.com>
2024-08-23 11:14:46 +0100

    example: cubit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,41 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() {
+    emit(state + 1);
+    // addError(Exception('increment error!'), StackTrace.current);
+  }
+
+  // @override
+  // void onChange(Change<int> change) {
+  //   super.onChange(change);
+  // }
+
+  // @override
+  // void onError(Object error, StackTrace stackTrace) {
+  //   super.onError(error, stackTrace);
+  // }
+}
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
@@ -17,4 +52,14 @@ void main() async {
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
   runApp(MyApp(settingsController: settingsController));
+
+  Bloc.observer = SimpleBlocObserver();
+  CounterCubit()
+    ..increment()
+    ..increment()
+    ..increment()
+    ..increment()
+    ..increment()
+    ..increment()
+    ..close();
 }
